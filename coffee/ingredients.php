df115400913002147927baa8da791f6426270a2c
Seireinity <79955262+Seireinity@users.noreply.github.com>
2022-09-02 00:31:12 +0800

    add ingredients

---
+++ coffee/ingredients.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Ingredients</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/e5a24515ea.js" crossorigin="anonymous"></script>
+
+        <body>
+            
+        </body>
+    </head>
+</html>
+    
